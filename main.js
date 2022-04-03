a9e645d670ebe760022dc84c3d1b3ff75779f316
Andrea Pontillo <andreapontillo97@gmail.com>
2022-04-03 17:48:41 +0200

    accomplished milestone 1 2 e 3

--- main.js
+++ main.js
@@ -21,13 +21,33 @@ var app = new Vue(
                 }
             ],
 
+            nuovoTodo: '',
         },
 
         methods: {
 
             removeTodo : function (todoIndex) {
                 this.todoList.splice (todoIndex,1)
+            },
+
+            addTodo : function (){
+
+                let oggTodo = {
+                    text: this.nuovoTodo,
+                    done: false
+                }
+                if (this.nuovoTodo.length > 0) {
+                    this.todoList.push(oggTodo)
+                    this.nuovoTodo = ''
+                } else {
+                    alert("Attenzione a quello che hai scritto come nuovo todo!")
+                }
+            },
+
+            done : function (element) {
+                element.done = true;
             }
+
         },
     }
 ) 
